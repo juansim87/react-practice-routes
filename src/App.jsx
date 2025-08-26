7cd1826e61f3e3fa3e37cdf8e4f10ed4e92cbadc
Juan Simon Abad <juansim87@gmail.com>
2025-08-26 12:15:51 +0200

    feat(App): add navigation bar and styles to highlight active link

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import "./styles.css";
 import { HomePage } from "./pages/HomePage";
 import { AboutPage } from "./pages/AboutPage";
+import { NavBar } from "./components/NavBar";
 
 export const App = () => {
 	return (
 		<div className="app-container">
 			<main className="main">
+      <NavBar/>
 				<h1>React Routes Practice</h1>
 				<Routes>
 					<Route path="/" element={<HomePage />} />
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import { NavLink } from "react-router-dom";
+
+export const NavBar = () => {
+	return (
+		<nav className="navbar">
+			<div className="navbar-items">
+				<NavLink to="/" className="navbar-item">Home</NavLink>
+				<NavLink to="/about" className="navbar-item">About</NavLink>
+			</div>
+		</nav>
+	);
+};
